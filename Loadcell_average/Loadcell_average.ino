b61c46791af426218610f428a3933b3fbbd1942e
Choi Yoon Seop <98401825+choiprin@users.noreply.github.com>
2022-04-14 11:25:05 +0900

    Add files via upload

---
+++ Loadcell_average/Loadcell_average.ino
@@ -0,0 +1,40 @@
+#include "HX711.h"
+ 
+#define calibration_factor 34500.0
+#define LOADCELL_DOUT_PIN D5
+#define LOADCELL_SCK_PIN D6
+int nValueCount = 10;
+float fValue[10];
+ 
+HX711 scale;
+
+void setup() 
+{
+  Serial.begin(9600);
+  Serial.println("HX711 kg demo");
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  scale.set_scale(calibration_factor);
+  scale.tare();
+}
+ 
+void loop() 
+{
+  Serial.print("Reading: ");
+  float fValueTemp = 0.0;
+  float fValueSum = 0.0;
+  fValueTemp = scale.get_units();
+  fValueSum = fValueTemp; 
+  int i;
+  for(i = 0; i < nValueCount; i = i + 1)
+  {
+    if(i > 0)
+    {
+      fValueSum = fValueSum + fValue[i];
+      fValue[i - 1] = fValue[i];
+    }
+  }
+  fValue[nValueCount - 1] = fValueTemp;
+  Serial.print((fValueSum / nValueCount), 1);
+  Serial.print(" kg");
+  Serial.println();
+}
